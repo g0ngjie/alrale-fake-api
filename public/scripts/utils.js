8f8f163a6f37d646401c3a4abb2e6089492ffa46
Gj <gongjie0422@163.com>
2021-05-24 20:56:06 +0800

    fix: modify

--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -1,5 +1,5 @@
 // 接口请求工具
-export async function request(path, method = "get", params = "", body = {}) {
+export async function request(path, method = "get") {
   // const url = "http://localhost:3000";
   // path = url + path;
   const res = await fetch(path, { method });
