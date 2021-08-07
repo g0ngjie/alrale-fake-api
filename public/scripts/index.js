197502577cb8b2e49258e8971bb3b40b3b8d450c
Gj <gongjie0422@163.com>
2021-08-07 20:26:32 +0800

    fix: cdn vue

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,4 @@
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js'
 import Routers from "./router.js";
 import { request, download, upload, getBaseUrl } from "./utils.js";
 
